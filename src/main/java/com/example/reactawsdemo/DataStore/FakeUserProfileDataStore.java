a6c3318c2916d1dd8e0b75207f2d9522032b2c21
aaronmccormack15533 <maron.accormack@gmail.com>
2020-08-21 12:39:00 +0100

    user profile model

---
+++ src/main/java/com/example/reactawsdemo/DataStore/FakeUserProfileDataStore.java
@@ -0,0 +1,24 @@
+package com.example.reactawsdemo.DataStore;
+
+import com.example.reactawsdemo.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+	private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+	static {
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "new user name", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "new user name2", null));
+		
+	}
+
+	public List<UserProfile> getUserProfiles(){
+		return USER_PROFILES;
+	}
+}
