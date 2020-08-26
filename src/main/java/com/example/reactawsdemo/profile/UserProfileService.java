35fb1ba1d163c04831685805b30a941bc1c49189
aaronmccormack15533 <maron.accormack@gmail.com>
2020-08-26 16:47:06 +0100

    frontend start

--- src/main/java/com/example/reactawsdemo/profile/UserProfileService.java
+++ src/main/java/com/example/reactawsdemo/profile/UserProfileService.java
@@ -3,8 +3,10 @@ package com.example.reactawsdemo.profile;
 import com.example.reactawsdemo.filestore.FileStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -19,5 +21,9 @@ public class UserProfileService {
 
 	List<UserProfile> getUserProfiles(){
 		return userProfileDataAccessService.getUserProfiles();
+	}
+
+	public void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
+
 	}
 }
